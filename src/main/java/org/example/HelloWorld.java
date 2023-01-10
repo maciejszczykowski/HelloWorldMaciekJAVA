4bfa7f4be190e382db9c70acc05962a75c12edf6
Maciej Szczykowski <maciej.szczykowski@gmail.com>
2023-01-10 14:57:06 +0100

    First commit

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Maciej!!");
+        System.out.println("Hello, World! Maciej!!!");
     }
 }
