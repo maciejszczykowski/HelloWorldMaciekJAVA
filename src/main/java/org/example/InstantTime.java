6d493dc94de93f2b6da6c5fedf2c78fc2a51e194
Maciej Szczykowski <maciej.szczykowski@gmail.com>
2023-01-10 14:56:13 +0100

    First commit

---
+++ src/main/java/org/example/InstantTime.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.time.Instant;
+import java.util.Date;
+public class InstantTime {
+    public static void main(String[] args) {
+        System.out.println("Aktualna godzina:");
+        System.out.println(Instant.now());
+    }
+}
